cdde95822eecf1bfdf42dec08db731288c922d31
Fardins_world <fardinprodhan.cse@gmail.com>
2026-07-04 01:07:32 +0600

    karenAndMorning.cpp

--- karenAndMorning.cpp
+++ karenAndMorning.cpp
@@ -1,46 +1,39 @@
-// Bismillahir Rahmanir Rahim
-#include <bits/stdc++.h>
+// with the name of Allah
+
+#include<bits/stdc++.h>
 using namespace std;
 
-bool palindrome(int h, int m)
-{
-    int h1 = h / 10;
-    int h2 = h % 10;
-    int m1 = m / 10;
-    int m2 = m % 10;
+bool palindrom(int h, int m){
 
-    return (h1 == m2 && h2 == m1);
+    return(h/10==m%10 && h%10==m/10);
+
 }
 
-int main()
-{
-    string s;
-    cin >> s;
+int main(){
 
-    int h = (s[0] - '0') * 10 + (s[1] - '0');
-    int m = (s[3] - '0') * 10 + (s[4] - '0');
+    string t;
+    cin>>t;
+
+    int h = stoi(t.substr(0,2));
+    int m = stoi(t.substr(3,2));
 
     int ans = 0;
 
-    while (true)
-    {
-        if (palindrome(h, m))
-        {
-            cout << ans << "\n";
+    while(true){
+        if(palindrom(h,m)){
+            cout<<ans;
             break;
         }
-
         ans++;
         m++;
 
-        if (m == 60)
-        {
-            m = 0;
+        if(m==60){
+            m=0;
             h++;
         }
-
-        if (h == 24)
-            h = 0;
+        if(h==24){
+            h=0;
+        }
     }
 
     return 0;
